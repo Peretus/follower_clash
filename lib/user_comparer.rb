31996a3b714e2302f9342c87168deb1e3d36e007
Peretus <caseymcneil@gmail.com>
2013-11-29 19:27:40 -0600

    Add foreman and keys to .env file for twitter API access.  Also add method for requesting user follower-count.

--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -4,6 +4,19 @@ require 'twitter'
 
 module UserComparer
   class User
+    def initialize(username)
+      attr_reader :username
+      @username = username
+    end
+
+    def followers
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key         = ENV["consumer_key"]
+        config.consumer_secret      = ENV["consumer_secret"]
+        config.access_token         = ENV["access_token"]
+        config.access_token_secret  = ENV["access_token_secret"]
+      end
+    end
   end
 
   class Comparer
