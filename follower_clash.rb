ad8fe78273a70c69706f843d3f5cdc659342b5ca
Peretus <caseymcneil@gmail.com>
2013-11-29 23:16:46 -0600

    Add support for comparing followers, following, user with the most recent tweet, and the text of the last tweet sent.

--- follower_clash.rb
+++ follower_clash.rb
@@ -1,5 +1,19 @@
 require 'rubygems'
 require 'bundler/setup'
+$LOAD_PATH << '.'
+require "lib/user_comparer.rb"
 
-require_relative "lib/user_comparer"
+user1 = UserComparer::User.new("caseymcneil")
+user2 = UserComparer::User.new("techpeace")
+
+puts "#{user1.username}'s follower count is: #{user1.followers}"
+puts "#{user2.username}'s follower count is: #{user2.followers}"
+
+my_comparer = UserComparer::Comparer.new(user1, user2)
+puts "#{my_comparer.choose_follower_winner.capitalize} has more followers!"
+puts "#{my_comparer.choose_oldest_winner.capitalize} is older!"
+puts "#{my_comparer.choose_following_winner.capitalize} is following more!"
+puts "The most recent tweet was from #{my_comparer.most_recent_tweeter.username.capitalize}:\n\n\n"
+puts "#{my_comparer.most_recent_tweeter.last_tweet.capitalize}"
+
 
--- lib/user_comparer.rb
+++ lib/user_comparer.rb
@@ -1,28 +1,85 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'twitter'
+require 'foreman'
+require 'pry'
 
 module UserComparer
   class User
-    def initialize(username)
-      attr_reader :username
-      @username = username
-    end
+    attr_reader :username, :followers, :following, :created_at, :most_recent_tweeter, :last_tweet, :last_tweet_time
 
-    def followers
+    def initialize(username)
+      @username = username
+
       client = Twitter::REST::Client.new do |config|
         config.consumer_key         = ENV["consumer_key"]
         config.consumer_secret      = ENV["consumer_secret"]
         config.access_token         = ENV["access_token"]
         config.access_token_secret  = ENV["access_token_secret"]
       end
+      @followers = client.user(@username).followers_count
+      @following = client.user(@username).friends_count
+      @created_at = client.user(@username).created_at
+      @last_tweet = client.user(@username).status.text
+      @last_tweet_time = client.user(@username).status.created_at
+
+
     end
+
+    # def followers
+    #   client = Twitter::REST::Client.new do |config|
+    #     config.consumer_key         = ENV["consumer_key"]
+    #     config.consumer_secret      = ENV["consumer_secret"]
+    #     config.access_token         = ENV["access_token"]
+    #     config.access_token_secret  = ENV["access_token_secret"]
+    #   end
+  #     client.user(@username).followers_count
+  #   end
   end
 
   class Comparer
+    attr_reader :followers
     def initialize(user1, user2)
       @user1 = user1
       @user2 = user2
+    end
+
+    def choose_follower_winner
+      if @user1.followers > @user2.followers
+        @user1.username #"#{user1.username} has more followers!"
+      elsif @user2.followers > @user1.followers
+        @user2.username #"#{user2.username} has more followers!"
+      else 
+        "It's a tie! Both #{user2.username} and #{user1.username} have the same number of followers!"
+      end
+    end
+
+    def choose_following_winner
+      if @user1.following > @user2.following
+        @user1.username #"#{user1.username} has more followers!"
+      elsif @user2.following > @user1.following
+        @user2.username #"#{user2.username} has more followers!"
+      else 
+        "It's a tie! Both #{user2.username} and #{user1.username} are following the same number of users!"
+      end
+    end
+
+    def choose_oldest_winner
+      if @user1.created_at > @user2.created_at
+        @user1.username #"#{user1.username} has more followers!"
+      elsif @user2.created_at > @user1.created_at
+        @user2.username #"#{user2.username} has more followers!"
+      else 
+        "It's a tie! Both #{user2.username} and #{user1.username} are the same age!"
+      end
+    end
+
+    def most_recent_tweeter
+      if @user1.last_tweet_time > @user2.last_tweet_time
+        @user1 #"#{user1.username} has more followers!"
+      else
+        @user2
+      end
     end
   end
 end
